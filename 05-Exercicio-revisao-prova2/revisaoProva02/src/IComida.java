d9b1a4924422814dcf2f1f7ce3087d4262773fc7
Felipe Vieira <luizfelipevieira01@gmail.com>
2023-06-13 07:50:24 -0300

    ex02rev

---
+++ 05-Exercicio-revisao-prova2/revisaoProva02/src/IComida.java
@@ -0,0 +1,6 @@
+import java.math.BigDecimal;
+
+public interface IComida {
+    public BigDecimal precoFinal()
+
+}
--- 05-Exercicio-revisao-prova2/revisaoProva02/src/IComidaVendivel.java
+++
@@ -1,7 +0,0 @@
-import java.math.BigDecimal;
-
-public interface IComidaVendivel {
-    public String toString()
-    public BigDecimal precoFinal()
-    public int compareTo(Comida o)
-}
